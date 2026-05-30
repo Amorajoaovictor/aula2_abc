ca1998face957ff3431b4ab274d88048a84cb1bd
amorajoaovictor <amorajoaovictor@gmail.com>
2026-05-30 08:37:12 -0300

    add exercise to calculate average of two grades

---
+++ exercicios_resolvidos/exercicio0.py
@@ -0,0 +1,9 @@
+# calcular a media de 2 notas 
+# perguntar o nome do aluno 
+# perguntar nota 1 
+# perguntar nota 2
+# calcular media 
+nota1 = float(input("diga sua nota"))
+nota2 = float(input("diga sua nota"))
+media = (nota1 + nota2) /2 
+print(f"sua nota é {media}")
